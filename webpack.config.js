66f51035ca668ca6715385dc7d67b17621e0e860
Michał Adamczyk <michal.anks@gmail.com>
2018-08-24 17:56:42 +0200

    Clean webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,10 @@ const path = require('path');
 const webpack = require('webpack');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
-const isWebpackDevServer = process.argv.some(a => path.basename(a) === 'webpack-dev-server');
-
-const isWatch = process.argv.some(a => a === '--watch');
-
-const plugins =
-  isWebpackDevServer || !isWatch ? [] : [
-    function(){
-      this.plugin('done', function(stats){
-        process.stderr.write(stats.toString('errors-only'));
-      });
-    }
-  ]
-;
-
 module.exports = {
   mode: 'development',
 
-
   devtool: 'original-source',
-  // devtool: 'eval-source-map',
-
-  devServer: {
-    contentBase: '.',
-    port: 4008,
-    stats: 'errors-only'
-  },
 
   entry: './app.js',
 
@@ -41,38 +19,4 @@ module.exports = {
   plugins: [
     new HardSourceWebpackPlugin()
   ]
-
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.purs$/,
-  //       use: [
-  //         {
-  //           loader: 'purs-loader',
-  //           options: {
-  //             src: [
-  //               'bower_components/purescript-*/src/**/*.purs',
-  //               'src/**/*.purs'
-  //             ],
-  //             bundle: false,
-  //             psc: 'psa',
-  //             watch: isWebpackDevServer || isWatch,
-  //             pscIde: true
-  //           }
-  //         }
-  //       ]
-  //     },
-  //   ]
-  // },
-
-  // resolve: {
-  //   modules: [ 'node_modules', 'bower_components' ],
-  //   extensions: [ '.purs', '.js']
-  // },
-
-  // plugins: [
-  //   new webpack.LoaderOptionsPlugin({
-  //     debug: false
-  //   })
-  // ].concat(plugins)
 };
